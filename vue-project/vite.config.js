f24109dcf21c0b0c968b9f91d71ce380c6fad3fb
fikriansyah-31 <syafanurwalinda23@gmail.com>
2023-09-25 16:08:56 +0700

    first commit

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -1,16 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Determine the base URL based on the GitHub Pages URL
-const base =
-  process.env.NODE_ENV === 'production'
-    ? '/imagelibrary.github.io/' // Use the repository name as the base
-    : '/';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base, // Set the base URL
   plugins: [
     vue(),
   ],
